d58564c64b72fd51b89e03a330d56fda679f07b6
obinnanprogrammed <obinnanwakwue10@gmail.com>
2024-03-23 11:01:31 -0500

    let's try this again

--- build4good-frontend/src/App.vue
+++ build4good-frontend/src/App.vue
@@ -1,16 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <WebsiteHeader />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import WebsiteHeader from './components/WebsiteHeader.vue'
+
 export default {
   name: 'App',
   components: {
-    HelloWorld, WebsiteHeader
+    HelloWorld
   }
 }
 </script>
