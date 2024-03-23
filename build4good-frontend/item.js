cc3234aa85d30ce0777265981a91cdacd9205a49
obinnanprogrammed <obinnanwakwue10@gmail.com>
2024-03-23 15:01:54 -0500

    data display fixed

--- build4good-frontend/item.js
+++ build4good-frontend/item.js
@@ -10,22 +10,21 @@ data = [];
 axios.get('http://localhost:3000/fetchData').then(response => {
     data = response.data;
     console.log(data);
+    for (let i = 0; i < num_items; i++) {
+        
+        const newListItem = document.createElement("div");
+    
+        newListItem.textContent;
+        newListItem.classList.add("item-box");
+    
+        newListItem.innerHTML = "<img src='images/bowl.png' class='item-img'>" 
+                                    + "<h1 class='item-title'>" + data[i].properties.Name.title[0].plain_text + "</h1>"
+                                    + "<p class='item-text'>Description</p>"
+                                    + "<p class='item-cost'>Price: " + data[i].properties.Price.number + "</p>"
+                                    + "<div class='item-button'>View Button</div>";
+        menu_div.appendChild(newListItem);
+    }
   })
   .catch(error => {
     console.error('Error fetching data:', error);
   });
-
-for (let i = 0; i < num_items; i++) {
-    console.log(data[i].properties);
-    const newListItem = document.createElement("div");
-
-    newListItem.textContent;
-    newListItem.classList.add("item-box");
-
-    newListItem.innerHTML = `<img src='images/bowl.png' class='item-img'>" 
-                                + "<h1 class='item-title'>" + data[i].properties.Name.title[0].plain_text + "</h1>"
-                                + "<p class='item-text'>Description</p>"
-                                + "<p class='item-cost'>Price: " + data[i].properties.Price.number + "</p>"
-                                + "<div class='item-button'>View Button</div>`;
-    menu_div.appendChild(newListItem);
-}
