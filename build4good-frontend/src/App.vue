7f884cd83f57c508db7cecfa4c46d70cd235b8d0
obinnanprogrammed <obinnanwakwue10@gmail.com>
2024-03-23 09:58:21 -0500

    added header template

--- build4good-frontend/src/App.vue
+++ build4good-frontend/src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
+  <WebsiteHeader />
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import WebsiteHeader from './components/WebsiteHeader.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld, WebsiteHeader
   }
 }
 </script>
